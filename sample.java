eef48fab13e4195a5899fa5274d38dfc519fb4b6
rahil <rahil@github.com>
2019-09-09 10:04:33 +0000

    commit1

--- sample.java
+++ sample.java
@@ -5,7 +5,6 @@ public static void main(String args[]){
 
 	System.out.println("Jenkins Automation Server");
 	System.out.println("Managed by Rahil - DBA Architect");
-	System.out.println("Code Remodified");
 }
 
 
