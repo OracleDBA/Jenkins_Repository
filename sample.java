1f8577d8485db1273296dd1a8a008df4151d72f5
rahil <rahil@github.com>
2019-09-09 10:47:47 +0000

    webhook concepts

--- sample.java
+++ sample.java
@@ -3,7 +3,7 @@ class sample{
 public static void main(String args[]){
 	System.out.println("Jenkins Automation Server");
 	System.out.println("Managed by Rahil - DBA Architect");
-	System.out.println("WebHooks - Configured - Code checked automatically during a commit"
+	System.out.println("WebHooks - Configured - Code checked automatically during a commit");
 }
 
 
